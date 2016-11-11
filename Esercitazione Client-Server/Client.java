6cbfb4b9264b75f1046fe292af6e2354a7e9c291
mattiaZonelli <mattiazonelli@gmail.com>
2016-11-11 09:39:00 +0100

    Client_Server 11.11.16

---
+++ Esercitazione Client-Server/Client.java
@@ -0,0 +1,46 @@
+package esercitazione1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ @author mattia.zonelli
+ */
+public class Client {
+	Socket con;
+	BufferedReader br;
+	PrintWriter out;
+
+	public static void main(String[] args) throws IOException {
+		Client client = new Client();
+		client.run();
+	}
+
+	public void run() throws IOException {
+
+		con = new Socket("localhost", 9999);
+		br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		out = new PrintWriter(con.getOutputStream(), true );
+		Scanner tast;
+		tast = new Scanner(System.in);
+		String s = "";
+		while (true) {
+			s = tast.nextLine();
+			if (s.equals("fine")) {
+				break;
+			}
+			out.println(s);
+			s = br.readLine();
+			System.out.println(s);
+
+		}
+		con.close();
+
+	}
+
+}
