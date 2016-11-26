863e1d93b291f04f717af4f8e181d47bd70365d5
safadienour <nour.safadie123@gmail.com>
2016-11-26 12:34:45 +0100

    Add files via upload
    Recupero del primo esercizio senza i thread

--- Client.java
+++ Client.java
@@ -1,64 +1,76 @@
-package echoserver;
 /*
- Author Crocetta Jacopo
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package client;
 
+/**
+ *
+ * @author Nour Safadie
+ */
 import java.net.*;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 public class Client {
-	Socket conn;
-	BufferedReader ingresso;
-	PrintStream uscita;
-	OutputStream out;
-	BufferedReader tastiera;
-
-	public Client() {
-		try {
-			conn = new Socket("localhost", 50000);
-			ingresso = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			out = conn.getOutputStream();
-			uscita = new PrintStream(conn.getOutputStream(), true);
-			tastiera = new BufferedReader(new InputStreamReader(System.in));
-		} catch (IOException ex) {
-			System.out.println("IO Exception");
-		}
-	}
-
-	public void send() {
-		String messaggio = "";
-		System.out.println("Inserisci il messaggio da inviare al server, per interrompere scrivere fine");
-		while (true) {
-			try {
-				messaggio = tastiera.readLine();
-				System.out.println(messaggio);
-			} catch (IOException ex) {
-				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-			}
-			if (messaggio.equals("fine")) {
-				System.out.println("Connessione scaduta");
-				try {
-					conn.close();
-					break;
-				} catch (IOException ex) {
-					System.out.println("Connessione scaduta");
-					Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			}
-			uscita.println(messaggio);
-			try {
-				System.out.println("ECHO: " + ingresso.readLine());
-				uscita.flush();
-			} catch (IOException ex) {
-				Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		Client c = new Client();//Nel costruttore instauro la connessione con il server, inizializzo il canale socket e i vari reader 
-		c.send();//Qui mando il messaggio che voglio per aspettare la risposta del server
-	}
+    
+    Socket conn;
+    BufferedReader ingresso;
+    PrintStream uscita;
+    OutputStream out;
+    BufferedReader tastiera;
+    
+    public Client() {
+        try {
+            conn = new Socket("localhost",30000);
+            ingresso = new BufferedReader (new InputStreamReader(conn.getInputStream()));
+            out = conn.getOutputStream();
+            uscita = new PrintStream(conn.getOutputStream(),true);
+            tastiera = new BufferedReader(new InputStreamReader(System.in));
+            
+        } catch (IOException ex) {
+            System.out.println ("IO Exception");
+        }
+     
+    }
+    public void send() {
+        String messaggio = "";
+        System.out.println("Digita il messaggio da inviare al server e per una fine di una comunicazione digita EXIT");
+        while (true) {
+            try {
+                messaggio = tastiera.readLine();
+                System.out.println(messaggio);
+            } catch (IOException ex) {
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                    
+            }
+            
+            if (messaggio.equals("end")) {
+                System.out.println("Connessione non è andata a buon fine");
+                try {
+                    conn.close();
+                    break;
+                } catch (IOException ex) {
+                    
+                    System.out.println("Si è verificato un errore nella chiusura della comunicazione");
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            uscita.println(messaggio);
+            try {
+                System.out.println ("ECHO: " + ingresso.readLine());
+                uscita.flush();
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException  {
+       
+        Client c = new Client();
+        c.send();
+    }
+    
 }
--- Server.java
+++ Server.java
@@ -1,53 +1,68 @@
-package echoserver;
 /*
- author Crocetta Jacopo
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package server;
 
+/**
+ *
+ * @author Nour Safadie
+ */
 import java.net.*;
 import java.io.*;
-
 public class Server {
-	ServerSocket s;
-	Socket conn;
-	OutputStream out;
-	PrintStream uscita;
-	InputStream input;
-	InputStreamReader in;
-	BufferedReader ingresso;
-	String messaggio;
-	String msn;
-
-	public Server() {
-		try {
-			s = new ServerSocket(50000);
-			conn = s.accept();
-			out = conn.getOutputStream();
-			uscita = new PrintStream(out);
-			input = conn.getInputStream();
-			in = new InputStreamReader(input);
-			ingresso = new BufferedReader(in);
-			messaggio = "messaggio ricevuto";
-			msn = "";
-		} catch (IOException ex) {
-			System.out.println("Errore");
-		} catch (NullPointerException ex) {
-			System.out.println("Errore");
-		}
-	}
-
-	public void receive() {
-		try {
-			msn = ingresso.readLine();
-			uscita.println(messaggio);
-		} catch (IOException ex) {
-			System.out.println("Errore");
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		Server c = new Server();
-		while (true) {
-			c.receive();
-		}
-	}
+    
+    ServerSocket s;
+    Socket conn;
+    OutputStream out;
+    PrintStream uscita;
+    InputStream input;
+    InputStreamReader in;
+    BufferedReader ingresso;
+    String Messaggio;
+    String MSN;
+    
+    public Server() {
+        try {
+            s= new ServerSocket(30000);
+            conn= s.accept();
+            out= conn.getOutputStream();
+            uscita = new PrintStream(out);
+            input= conn.getInputStream();
+            in= new InputStreamReader(input);
+            ingresso= new BufferedReader(in);
+            Messaggio ="messaggio ricevuto";
+            MSN = "";
+        } catch (IOException | NullPointerException ex) {
+            System.out.println("impossibile creare il server");
+            
+            
+        }
+    }
+    
+    public void Ricevere() {
+        try {
+            MSN = ingresso.readLine();
+            uscita.println(Messaggio);
+        } catch (IOException ex){
+            System.out.println (" nessun messaggio è stato ricevuto ");
+            
+       
+        }
+    }
+    
+     
+  
+    public static void main(String[] args) throws IOException { {
+        
+        Server s=new Server();
+        
+        while (true) {
+            s.Ricevere();
+        }
+    }
+   
+    }
+    
 }
